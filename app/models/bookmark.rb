8c24276385cb8a15e49e895646fba15db5c77255
Paul Lanza <paullanza@gmail.com>
2022-05-12 14:54:56 -0400

    Rake Green, starting HTML and CSS now

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  # validates :movie_id, :list_id, presence: true
   validates :comment, length: { minimum: 6 }
   validates :list_id, uniqueness: true
-  # validates :movie_id, uniqueness: { scope: :list_id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :lists, only: [:index, :new, :show, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  delete '/bookmarks/:id', to: 'bookmarks#destroy'
 end
