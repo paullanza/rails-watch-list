642aaea34d693c708820bfd028bc3202495ec4f9
Paul Lanza <paullanza@gmail.com>
2022-05-16 09:48:50 -0400

    Late push

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmarks = Bookmark.where(:list == @list)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :new, :show, :create] do
     resources :bookmarks, only: [:new, :create]
   end
-
-  delete '/bookmarks/:id', to: 'bookmarks#destroy'
+  root to: 'lists#index'
+  delete '/bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark'
 end
